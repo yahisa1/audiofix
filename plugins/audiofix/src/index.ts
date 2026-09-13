e759f5a4c11c89a7babfd46d3e45b6fd25d89ae7
asdf <131851700+yahisa1@users.noreply.github.com>
2026-09-13 21:56:51 +0900

    Update index.ts

--- plugins/audiofix/src/index.ts
+++ plugins/audiofix/src/index.ts
@@ -1,27 +1,18 @@
 import { ReactNative as RN } from "@vendetta/metro/common";
 
-export const onLoad = () => {
-    try {
-        // RN.NativeModules에 등록된 모든 모듈의 이름을 추출
-        const allModules = RN.NativeModules ? Object.keys(RN.NativeModules) : [];
-        
-        // 이름에 Audio, Voice, Media, Phone 등이 포함된 모듈 필터링
-        const audioRelated = allModules.filter(name => 
-            /audio|voice|media|phone|sound/i.test(name)
-        );
+const modules = RN.NativeModules;
 
-        console.log("All Native Modules:", allModules);
-        console.log("Audio Related Modules:", audioRelated);
+console.log("[AudioFix] AudioManager:", modules.AudioManager);
+console.log("[AudioFix] RTNAudioManager:", modules.RTNAudioManager);
 
-        if (audioRelated.length > 0) {
-            alert("찾은 관련 모듈:\n" + audioRelated.join(", "));
-        } else {
-            alert("관련 모듈 없음. 전체 모듈 개수: " + allModules.length);
-        }
-    } catch (e) {
-        console.error("스캔 중 에러:", e);
-        alert("에러: " + e.message);
-    }
-};
+console.log(
+    "[AudioFix] AudioManager keys:",
+    modules.AudioManager ? Object.keys(modules.AudioManager) : null
+);
+
+console.log(
+    "[AudioFix] RTNAudioManager keys:",
+    modules.RTNAudioManager ? Object.keys(modules.RTNAudioManager) : null
+);
 
 export const onUnload = () => {};
